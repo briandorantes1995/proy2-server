5358b4646dc67c3775e90dc0a7bba68e72002341
¨BrianDorantes¨ <brian_dorantes@icloud.com>
2023-09-24 18:17:57 -0600

    se termino login y se agregaron rutas de habitaciones{crearhabitacion,mostrarHabitaciones, tipos de habitaciones, mostrar tipos}

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
-from flask import Flask, request, Response
+
+import jwt
+from flask import Flask, request, Response, make_response, jsonify
 from flask_cors import CORS
 from prisma import Prisma
 from dotenv import load_dotenv
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from datetime import datetime, timedelta
 
 load_dotenv()
 
@@ -17,6 +19,8 @@ CORS(app)
 def hello_world():
     return 'Inicio Proy Int 2'
 
+
+# Usuario
 
 @app.route('/registro', methods=['POST'])
 async def registro_usuario():
@@ -36,14 +40,137 @@ async def registro_usuario():
 
 
 @app.route('/login', methods=['POST'])
-def login_usuario():
-    db.connect()
-    db.usuario.find_unique(
+async def login_usuario():
+    auth = request.form
+
+    if not auth or not auth.get('email') or not auth.get('password'):
+        return make_response(
+            'Could not verify', 401, {'WWW-Authenticate': 'Basic realm ="Login required !!"'})
+    db = Prisma()
+    await db.connect()
+    user = await db.usuario.find_unique(
         where={
             'email': request.form['email'],
         }
     )
-    return Response("Usario Creado Exitosamente", status=200)
+
+    if not user:
+        return make_response(
+            'Could not verify', 401, {'WWW-Authenticate': 'Basic realm ="User does not exist !!"'})
+
+    if check_password_hash(user.password, auth.get('password')):
+        token = jwt.encode({
+            'nombre': user.nombre,
+            'email': user.email,
+            'exp': datetime.utcnow() + timedelta(minutes=30)
+        }, app.config['SECRET_KEY'])
+        return make_response(jsonify({'token': token}), 201)
+
+    return make_response('Could not verify', 403, {'WWW-Authenticate': 'Basic realm ="Wrong Password !!"'})
+
+
+@app.route('/mostrarUsuarios', methods=['GET'])
+async def mostrar_usuarios():
+    db = Prisma()
+    await db.connect()
+    usuarios = await db.usuario.find_many()
+    if usuarios:
+        usuarios_serializados = []
+        for usuario in usuarios:
+            usuario_serializado = {
+                'id': usuario.id,
+                'nombre': usuario.nombre,
+                'email': usuario.email,
+                'hospedado_actualmente': usuario.hospedado,
+                'cuarto': usuario.cuarto,
+            }
+            usuarios_serializados.append(usuario_serializado)
+        return make_response(jsonify({'usuarios': usuarios_serializados}), 201)
+    else:
+        return Response("No se pueden mostrar los usuarios", status=400)
+
+
+# Habitaciones----------------------------------------------------------------------------------------------------------
+@app.route('/crearTipo', methods=['POST'])
+async def crear_tipo():
+    db = Prisma()
+    await db.connect()
+    tipo = await db.tipohabitacion.create(
+        data={
+            'tipo': request.form['nombre'],
+            'descripcion': request.form['descripcion'],
+        },
+    )
+    if tipo:
+        return Response("Se ha registrado Tipo de habitacion", status=200)
+    else:
+        return Response("No se pudo crear el tipo de habitacion", status=400)
+
+
+@app.route('/Tipos', methods=['GET'])
+async def mostrar_tipos():
+    db = Prisma()
+    await db.connect()
+    tipos = await db.tipohabitacion.find_many()
+
+    if tipos:
+        tipos_serializados = []
+        for tipo in tipos:
+            tipo_serializado = {
+                'id': tipo.id,
+                'tipo': tipo.tipo,
+                'descripcion': tipo.descripcion,
+            }
+            tipos_serializados.append(tipo_serializado)
+        return make_response(jsonify({'tipos': tipos_serializados}), 201)
+    else:
+        return Response("No se pueden mostrar los tipos de habitacion", status=400)
+
+
+@app.route('/crearHabitacion', methods=['POST'])
+async def crear_habitacion():
+    db = Prisma()
+    await db.connect()
+    tipo_habitacion = await db.tipohabitacion.find_first(where={"tipo": request.form['tipo']})
+    habitacion = await db.habitaciones.create(
+        data={
+            'numero': request.form['numero'],
+            'costo': request.form['costo'],
+            'tipo': {
+                'connect': {
+                    'id': tipo_habitacion.id
+                }
+            },
+        },
+    )
+    if habitacion:
+        return Response("Se ha registrado la habitacion", status=200)
+    else:
+        return Response("No se pudo registrar/crear la habitacion", status=400)
+
+
+@app.route('/Habitaciones', methods=['GET'])
+async def mostrar_habitaciones():
+    db = Prisma()
+    await db.connect()
+    habitaciones = await db.habitaciones.find_many()
+    if habitaciones:
+        habitaciones_serializados = []
+        for habitacion in habitaciones:
+            tipo_habitacion = await db.tipohabitacion.find_first(
+                where={"id": habitacion.tipohabitacionId}
+            )
+            habitacion_serializado = {
+                'id': habitacion.id,
+                'numero': habitacion.numero,
+                'tipo': tipo_habitacion.tipo,
+                'descripcion': tipo_habitacion.descripcion,
+                'costo': habitacion.costo,
+            }
+            habitaciones_serializados.append(habitacion_serializado)
+        return make_response(jsonify({'habitaciones': habitaciones_serializados}), 201)
+    else:
+        return Response("No se pueden mostrar los tipos de habitacion", status=400)
 
 
 if __name__ == '__main__':
